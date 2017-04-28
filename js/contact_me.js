3e7a8a83e7e7537b0d6c60001acc8055260a2547
Chanchur Bansal <chanchurbansal@users.noreply.github.com>
2017-04-29 03:56:19 +0530

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,7 +17,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/chanchurplay37@gmail.com@gmail.com",
+                url: "https://formspree.io/sakshi7896@gmail.com",
                 method: "POST",
                 data: {
                     name: name,
